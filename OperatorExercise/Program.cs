77ed4900bc0d41ed3a17a7b4f2f8041d7530e38b
Daniel Finch <xiphoid0311@gmail.com>
2023-05-29 19:43:48 -0500

    Created method for area of circle with users radius

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //For division create two integer variables a and b.
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+            if(a == 17 && b == 4)
+            {
+                
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+                Console.WriteLine("What is the radius of your circle?");
+
+
+                var radius = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"The area of your circle with a radius of {radius} is {AreaOfCircle(radius)}");
+            }
         }
+        //Define a method which will calculate the area of a circle based on its radius. 
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+        }
+
     }
 }
